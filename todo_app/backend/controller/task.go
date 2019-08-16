03c3189fd2477dacca7c1aa5fc876e37007c431b
ryoya_ito <horusu.dqx@gmail.com>
2019-08-16 16:34:38 +0900

    タスク全取得メソッド実装

--- todo_app/backend/controller/task.go
+++ todo_app/backend/controller/task.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/ry-itto/treasure-app/todo_app/backend/repository"
 )
 
 type Task struct {
@@ -15,5 +16,6 @@ func NewTaskController(db *sqlx.DB) *Task {
 }
 
 func (t *Task) Index(w http.ResponseWriter, r *http.Request) (int, interface{}, error) {
-	return http.StatusNotImplemented, nil, nil
+	tasks, err := repository.FindAllTasks(t.db)
+	return http.StatusOK, tasks, err
 }
