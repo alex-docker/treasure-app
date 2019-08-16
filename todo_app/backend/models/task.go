c8decc96686b5a37eb97094ed18c6ef4e8a6bef1
ryoya_ito <horusu.dqx@gmail.com>
2019-08-16 16:19:44 +0900

    タスクモデル作成

---
+++ todo_app/backend/models/task.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Task struct {
+	ID        int       `db:"id" json:"id"`
+	Title     string    `db:"title" json:"title"`
+	Content   string    `db:"content" json:"content"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+}
