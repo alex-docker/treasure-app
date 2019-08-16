8f2eb4d6f4dbf8620992b5f0d1642cc416603858
ryoya_ito <horusu.dqx@gmail.com>
2019-08-16 14:08:38 +0900

    TODOAPI作成開始/main作成

---
+++ todo_app/backend/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	server "github.com/ry-itto/treasure-app/todo_app/server"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	log.SetFlags(log.Ldate + log.Ltime + log.Lshortfile)
+	log.SetOutput(os.Stdout)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("error loading .env file. %s", err)
+	}
+
+	datasource := os.Getenv("DATABASE_DATASOURCE")
+	if datasource == "" {
+		log.Fatal("Cannot get datasource for database.")
+	}
+
+	s := server.NewServer()
+	s.Init(datasource)
+	s.Run(os.Getenv("PORT"))
+}
